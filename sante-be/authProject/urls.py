57a128a560ebbc6477cc5600ba9409e6ace247f9
Aida Mina <aida.mina@correounivalle.edu.co>
2022-09-20 00:04:17 -0500

    Return project to previous state

--- sante-be/authProject/urls.py
+++ sante-be/authProject/urls.py
@@ -4,6 +4,4 @@ from authApp import views
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
-    path('user/', views.UserCreateView.as_view()),
-    path('user/<int:pk>/', views.UserDetailView.as_view()),
 ]
