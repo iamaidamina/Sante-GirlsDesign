52251e4001adbdfff2dded663086156ed796e3fe
Aida Mina <aida.mina@correounivalle.edu.co>
2022-09-20 00:29:43 -0500

    Returning project to previous state

--- sante-be/authProject/urls.py
+++ sante-be/authProject/urls.py
@@ -1,7 +1,6 @@
+from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from authApp import views
+
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view()),
-    path('refresh/', TokenRefreshView.as_view()),
+    path('admin/', admin.site.urls),
 ]
